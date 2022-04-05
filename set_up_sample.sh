4be77a6893d113381a85d4d425699164b93f8d09
agherashyam2000 <53151728+agherashyam2000@users.noreply.github.com>
2022-04-05 16:05:04 +0530

    Update set_up_sample.sh

--- set_up_sample.sh
+++ set_up_sample.sh
@@ -4,36 +4,16 @@ set -e
 set -o pipefail
 
 # get mpi ref and set up openfoam env
-OPENFOAM_DIR=/opt/OpenFOAM/OpenFOAM-4.0
+#OPENFOAM_DIR=/opt/OpenFOAM/OpenFOAM-4.0
 source /etc/profile.d/modules.sh
-module add mpi/openmpi-x86_64
-source $OPENFOAM_DIR/etc/bashrc
-
-# copy sample into auto scratch shared area
-#AUTO_SCRATCH_DIR=$AZ_BATCH_TASK_DIR/auto_scratch
-#cd $AUTO_SCRATCH_DIR
-#cp -r $OPENFOAM_DIR/tutorials/incompressible/simpleFoam/pitzDaily .
-#cp $OPENFOAM_DIR/tutorials/incompressible/simpleFoam/pitzDailyExptInlet/system/decomposeParDict pitzDaily/system/
+#module add mpi/openmpi-x86_64
+source /usr/lib/openfoam/openfoam2106/etc/bashrc
 
 # get nodes and compute number of processors
 IFS=',' read -ra HOSTS <<< "$AZ_BATCH_HOST_LIST"
 nodes=${#HOSTS[@]}
 ppn=`nproc`
 np=$(($nodes * $ppn))
-
-# substitute proper number of subdomains
-#sed -i -e "s/^numberOfSubdomains 4/numberOfSubdomains $np;/" pitzDaily/system/decomposeParDict
-#root=`python -c "import math; x=int(math.sqrt($np)); print x if x*x==$np else -1"`
-#if [ $root -eq -1 ]; then
-#    sed -i -e "s/\s*n\s*(2 2 1)/    n               ($ppn $nodes 1)/g" pitzDaily/system/decomposeParDict
-#else
-#    sed -i -e "s/\s*n\s*(2 2 1)/    n               ($root $root 1)/g" pitzDaily/system/decomposeParDict
-#fi
-
-# decompose
-#cd pitzDaily
-#blockMesh
-#decomposePar -force
 
 # create hostfile
 hostfile="hostfile"
@@ -46,7 +26,5 @@ done
 
 # export parameters
 export mpirun=`which mpirun`
-export mpienvopts=`echo \`env | grep WM_ | sed -e "s/=.*$//"\` | sed -e "s/ / -x /g"`
-export mpienvopts2=`echo \`env | grep FOAM_ | sed -e "s/=.*$//"\` | sed -e "s/ / -x /g"`
 export np
 export hostfile
