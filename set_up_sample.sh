40fb9ad4d283e4c2578990a360ec794992361ea5
agherashyam2000 <53151728+agherashyam2000@users.noreply.github.com>
2022-04-06 12:08:22 +0530

    Update set_up_sample.sh

--- set_up_sample.sh
+++ set_up_sample.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #set -e
 #set -o pipefail
@@ -10,21 +10,21 @@ source /etc/profile.d/modules.sh
 source /usr/lib/openfoam/openfoam2106/etc/bashrc
 
 # get nodes and compute number of processors
-IFS=',' read -ra HOSTS <<< "$AZ_BATCH_HOST_LIST"
-nodes=${#HOSTS[@]}
-ppn=`nproc`
-np=$(($nodes * $ppn))
+#IFS=',' read -ra HOSTS <<< "$AZ_BATCH_HOST_LIST"
+#nodes=${#HOSTS[@]}
+#ppn=`nproc`
+#np=$(($nodes * $ppn))
 
 # create hostfile
-hostfile="hostfile"
-touch $hostfile
->| $hostfile
-for node in "${HOSTS[@]}"
-do
-    echo $node slots=$ppn max-slots=$ppn >> $hostfile
-done
+#hostfile="hostfile"
+#touch $hostfile
+#>| $hostfile
+#for node in "${HOSTS[@]}"
+#do
+#    echo $node slots=$ppn max-slots=$ppn >> $hostfile
+#done
 
 # export parameters
 #export mpirun=`which mpirun`
-export np
-export hostfile
+#export np
+#export hostfile
